89b1abae49cf6d1f0afe67d3125206ca0e242f3e
ImenHzl <166798845+ImenHzl@users.noreply.github.com>
2024-06-26 20:54:10 +0200

    Update test_historique.py

--- tests/test_historique.py
+++ tests/test_historique.py
@@ -1,5 +1,6 @@
 from app.api.v1.historique import Historique
 
+
 def test_arroseplante_jour_de_pluie():
     """
     Teste la fonction ArrosePlante avec un cas où il pleut tous les jours
@@ -12,18 +13,16 @@ def test_arroseplante_jour_de_pluie():
     # Exécution de la fonction
     data = Historique.ArrosePlante(pays, dt, endt)
 
-    # Vérification que data n'est pas None
-    assert data is not None, "La fonction ArrosePlante a retourné None, mais une liste était attendue."
-
     # Vérification du type de retour
-    assert isinstance(data, list), f"Le type retourné est {type(data)}, mais une liste était attendue."
+    assert isinstance(data, list)
 
     # Vérification du nombre de jours
-    assert len(data) == 1, f"Le nombre de jours retournés est {len(data)}, mais 1 était attendu."
+    assert len(data) == 1
 
     # Vérification des données du jour
     assert data[0]["date"] == "2024-06-15"
     assert data[0]["meteo du jour"] == "il pleut"
+
 
 def test_arroseplante_jour_sans_pluie():
     """
@@ -31,21 +30,18 @@ def test_arroseplante_jour_sans_pluie():
     """
     # Données d'entrée
     pays = "France"
-    dt = "2024-06-16"
-    endt = "2024-06-16"
+    dt = "2024-06-25"
+    endt = "2024-06-25"
 
     # Exécution de la fonction
     data = Historique.ArrosePlante(pays, dt, endt)
 
-    # Vérification que data n'est pas None
-    assert data is not None, "La fonction ArrosePlante a retourné None, mais une liste était attendue."
-
     # Vérification du type de retour
-    assert isinstance(data, list), f"Le type retourné est {type(data)}, mais une liste était attendue."
+    assert isinstance(data, list)
 
     # Vérification du nombre de jours
-    assert len(data) == 1, f"Le nombre de jours retournés est {len(data)}, mais 1 était attendu."
+    assert len(data) == 1
 
     # Vérification des données du jour
-    assert data[0]["date"] == "2024-06-16"
-    assert data[0]["meteo du jour"] == "il pleut"
+    assert data[0]["date"] == "2024-06-25"
+    assert data[0]["meteo du jour"] == "il pleut pas il faut arroser les plantes"
